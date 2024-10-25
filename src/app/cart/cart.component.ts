15ab07971dd0e48da31dafff7d05e0c8c34a6792
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-25 12:16:39 -0400

    cart service generated and function to handel cart http request added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IDevice } from '../device-details/device.model';
 
-@Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+@Injectable({
+  providedIn: 'root'
 })
-export class CartComponent {
+export class CartService {
+ private cartSubject = new BehaviorSubject<IDevice[]>([])
+ public cart$:Observable<IDevice[]>=this.cartSubject.asObservable()
 
+  constructor(private http:HttpClient) { 
+    this.getCart().subscribe((cartData)=>{
+        this.cartSubject.next(cartData)
+    })
+
+  }
+ 
+  public getCart():Observable<IDevice[]>{
+   return this.http.get<IDevice[]>("/api/carts")
+  }
+
+  // add device to cart 
+  public add(device:IDevice){
+      this.http.post("/api/carts",device).subscribe(()=>{
+      console.log(`Device ${device.model} added to cart`);
+      //refresh the cart 
+        this.getCart().subscribe((cartData)=>{
+        this.cartSubject.next(cartData)
+      })
+    })
+
+  }
+
+  public deleteDeviceFromCart(device:any){
+      this.http.delete(`/api/carts/${device.id}`).subscribe(()=>{
+      console.log(`Device ${device.model} removed from the cart`);
+      //refresh the cart
+        this.getCart().subscribe((cartData)=>{
+        this.cartSubject.next(cartData)
+      })
+    })
+  }
 }
