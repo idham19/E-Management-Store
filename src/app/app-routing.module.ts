9ca477915328bfb39b7fc16437e25fe7b1c16c6f
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-28 17:26:19 -0400

    html css styling for register component  and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { AddFormComponent } from './add-form/add-form.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-{path:"",redirectTo:'/home',pathMatch:"full"},
+  {path:"",redirectTo:'/home',pathMatch:"full"},
+{path:"signIn",component:SignInComponent},
+{path:"register",component:RegisterComponent},
 {path:"home",component:HomeComponent},
 {path:"cart",component:CartComponent},
 {path:"add-form",component:AddFormComponent},
