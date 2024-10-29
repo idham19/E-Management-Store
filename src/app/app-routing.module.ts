a2be853d55b8c647a79be166c570e62d826a6254
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-29 16:50:23 -0400

    method added to auth.service and app-routing updated with auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,18 @@ import { CartComponent } from './cart/cart.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { AddFormComponent } from './add-form/add-form.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { authGuard } from './auth.guard';
 import { RegisterComponent } from './register/register.component';
 
-
 const routes: Routes = [
-  {path:"",redirectTo:'/home',pathMatch:"full"},
-{path:"signIn",component:SignInComponent},
+{path:"",redirectTo:'/signIn',pathMatch:"full"},
+{path:"signIn",component:SignInComponent,},
 {path:"register",component:RegisterComponent},
-{path:"home",component:HomeComponent},
-{path:"cart",component:CartComponent},
-{path:"add-form",component:AddFormComponent},
-{path:"add-form/:id",component:AddFormComponent},
-{path:"catalog",component:CatalogComponent},
+{path:"home",component:HomeComponent ,canActivate:[authGuard]},
+{path:"cart",component:CartComponent,canActivate:[authGuard]},
+{path:"catalog",component:CatalogComponent,canActivate:[authGuard]},
+{path:"add-form",component:AddFormComponent,canActivate:[authGuard]},
+{ path: 'add-form/:id', component: AddFormComponent,canActivate:[authGuard] }
 
 ];
 
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+
+  private authStatus= new BehaviorSubject<boolean>(false);
+  public isLogginIn$ = this.authStatus.asObservable(); 
+
+
+  setAuthState(state: boolean) {
+    this.authStatus.next(state);
+  }
+
+  isLoggedIn(): boolean {
+    return this.authStatus.value;
+  }
 }
