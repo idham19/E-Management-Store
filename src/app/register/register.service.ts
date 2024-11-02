514aca9427c075a13f825468d079ac2a9bbe1e10
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-11-02 16:29:29 -0400

    environments folder with environment files  for local and production added

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class RegisterService {
   constructor(private http:HttpClient) { }
 
   public registerPostUser(email:string,password:string):Observable<any>{
-    return this.http.post("/api/auth/register",{email,password},{ responseType: 'text' })
+    return this.http.post(`${environment.apiUrl}/auth/register`,{email,password},{ responseType: 'text' })
     }
 }
--- src/app/sign-in/sign-in.service.ts
+++ src/app/sign-in/sign-in.service.ts
@@ -23,6 +23,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
@@ -32,7 +34,7 @@ export class SignInService {
   constructor(private http :HttpClient) { }
 
 public loginPostUser(email:string,password:string):Observable<any>{
-  return this.http.post("/api/auth/login",{email,password})
+  return this.http.post(`${environment.apiUrl}/auth/login`,{email,password})
 }
 
 public registerPostUser(email:string,password:string):Observable<any>{
