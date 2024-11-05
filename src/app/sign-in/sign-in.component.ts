e99ed08065ec58347bfcfc8604021aaed0935c45
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-11-05 16:24:50 -0500

    loading spinning added to signIn page

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -13,7 +13,7 @@ import { AuthService } from '../auth.service';
 export class SignInComponent {
 
  isLoggedIn=false
-
+ isLoading=false
  loginForm = new FormGroup({
  email: new FormControl('', [Validators.required, Validators.email]),
  password: new FormControl('', Validators.required)
@@ -34,20 +34,29 @@ export class SignInComponent {
 
   signIn() {
     if (this.loginForm.valid){
+      this.isLoading=true
        let email:any =this.loginForm.value.email;
        let password:any=this.loginForm.value.password
       this.signInSvc.loginPostUser(email, password).subscribe({
         next: () => {
+          this.isLoading=false
           console.log("Signed in successfully");
           this.authSvc.setAuthState(true);
           this.router.navigate(["/home"]);
         },
         error: error => {
+          this.isLoading=false
           alert("Incorrect Password/Email");
           console.error("Incorrect Password/Email", error);
         }
       });
     }
+    else {
+      this.isLoading=true
+      this.isLoading=false;
+      alert("Pleas Enter you Password and Email, or Register")
+    }
    }
+   
 
 }
